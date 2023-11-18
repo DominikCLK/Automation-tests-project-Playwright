e1437f79357cbbe65f9d103dc11cc4b254721bfe
DominikCLK <dominik.calak@gmail.com>
2023-11-18 18:29:49 +0100

    chore: remove console.log

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -2,6 +2,5 @@ import dotenv from 'dotenv';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
-  console.log(`!!!!! URL:`, process.env.BASE_URL);
 }
 export default globalSetup;
