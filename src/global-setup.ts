611809f6d182fd2299e814de285c8020914a1feb
DominikCLK <dominik.calak@gmail.com>
2023-11-18 18:28:22 +0100

    feat: dotenv for env var

---
+++ src/global-setup.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+
+async function globalSetup(): Promise<void> {
+  dotenv.config({ override: true });
+  console.log(`!!!!! URL:`, process.env.BASE_URL);
+}
+export default globalSetup;
