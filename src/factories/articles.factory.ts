e57b87a57d227238b78b094bf145c1d4c86d745b
DominikCLK <dominik.calak@gmail.com>
2023-11-28 13:53:44 +0100

    test: checking title length

--- src/factories/articles.factory.ts
+++ src/factories/articles.factory.ts
@@ -1,9 +1,16 @@
 import { AddArticleModel } from '../models/article.model';
 import { faker } from '@faker-js/faker/locale/en';
 
-export function randomNewArticle(): AddArticleModel {
-  const title = faker.lorem.sentence();
-  const body = faker.lorem.paragraphs(5);
+export function randomNewArticle(
+  titleLength?: number,
+  bodyParagraphs = 5,
+): AddArticleModel {
+  let title: string;
+
+  if (titleLength) title = faker.string.alpha(titleLength);
+  else title = faker.lorem.sentence();
+
+  const body = faker.lorem.paragraphs(bodyParagraphs);
 
   const newArticle: AddArticleModel = { title: title, body: body };
 
--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -1,5 +1,4 @@
 import { randomNewArticle } from '../src/factories/articles.factory';
-import { AddArticleModel } from '../src/models/article.model';
 import { ArticlePage } from '../src/pages/article.page';
 import { ArticlesPage } from '../src/pages/articles.page';
 import { LoginPage } from '../src/pages/login.page';
@@ -11,7 +10,6 @@ test.describe('Verify articles', () => {
   let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let addArticleView: AddArticleView;
-  let articleData: AddArticleModel;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -23,14 +21,13 @@ test.describe('Verify articles', () => {
     await articlesPage.goto();
     await articlesPage.addArticleButtonLogged.click();
 
-    articleData = randomNewArticle();
-
     await expect.soft(addArticleView.header).toBeVisible();
   });
 
   test('create new articles @GAD-R04-01', async ({ page }) => {
     // Arrange
     const articlePage = new ArticlePage(page);
+    const articleData = randomNewArticle();
 
     // Act
     await addArticleView.createArticle(articleData);
@@ -45,7 +42,6 @@ test.describe('Verify articles', () => {
   test('reject creating articles without title @GAD-R04-01', async () => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
-
     const articleData = randomNewArticle();
     articleData.title = '';
 
@@ -62,6 +58,7 @@ test.describe('Verify articles', () => {
   test('reject creating articles without body @GAD-R04-01', async () => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
+    const articleData = randomNewArticle();
     articleData.body = '';
 
     // Act
@@ -72,5 +69,33 @@ test.describe('Verify articles', () => {
     await expect(addArticleView.errorAlertPopup).toHaveText(
       expectedErrorMessage,
     );
+  });
+
+  test('reject creating articles without title exceeding 128 signs@GAD-R04-02', async () => {
+    // Arrange
+    const expectedErrorMessage = 'Article was not created';
+    const articleData = randomNewArticle(129);
+
+    // Act
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    await expect(addArticleView.errorAlertPopup).toHaveText(
+      expectedErrorMessage,
+    );
+  });
+
+  test('reject creating articles without title with 128 signs@GAD-R04-02', async ({
+    page,
+  }) => {
+    // Arrange
+    const articlePage = new ArticlePage(page);
+    const articleData = randomNewArticle(128);
+
+    // Act
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
   });
 });
