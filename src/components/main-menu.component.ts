7e4776226ae11755908342574dcfa782d0e2de52
DominikCLK <dominik.calak@gmail.com>
2024-01-19 16:35:04 +0100

    chore: use page object methods in menu component

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -1,9 +1,27 @@
+import { ArticlesPage } from '@_src/pages/articles.page';
+import { CommentsPage } from '@_src/pages/comments.page';
+import { HomePage } from '@_src/pages/home.page';
 import { Page } from '@playwright/test';
 
 export class MainMenuComponent {
   commentButton = this.page.getByTestId('open-comments');
   articlesButton = this.page.getByTestId('open-articles');
-  homePage = this.page.getByRole('link', { name: '🦎 GAD' });
+  homePageLink = this.page.getByRole('link', { name: '🦎 GAD' });
 
   constructor(private page: Page) {}
+
+  async clickCommentsButton(): Promise<CommentsPage> {
+    await this.commentButton.click();
+    return new CommentsPage(this.page);
+  }
+
+  async clickArticlesButton(): Promise<ArticlesPage> {
+    await this.articlesButton.click();
+    return new ArticlesPage(this.page);
+  }
+
+  async clickHomePageLink(): Promise<HomePage> {
+    await this.homePageLink.click();
+    return new HomePage(this.page);
+  }
 }
