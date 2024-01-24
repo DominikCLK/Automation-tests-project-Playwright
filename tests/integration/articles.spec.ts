af31dd0d41ce772f64a04826fd912f5b7122c0a9
DominikCLK <dominik.calak@gmail.com>
2024-01-24 19:18:56 +0100

    chore: backend verification for test

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,22 +1,31 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/articles.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articles', () => {
   test('reject creating article without title @GAD-R04-01 @logged', async ({
     addArticleView,
+    page,
   }) => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
+    const expectedResponseCode = 422;
+
     const articleData = prepareRandomArticle();
     articleData.title = '';
+    const responsePromise = page.waitForResponse('/api/articles', {
+      timeout: RESPONSE_TIMEOUT,
+    });
 
     // Act
     await addArticleView.createArticle(articleData);
+    const response = await responsePromise;
 
     // Assert
     await expect(addArticleView.errorAlertPopup).toHaveText(
       expectedErrorMessage,
     );
+    expect(response.status()).toBe(expectedResponseCode);
   });
 
   test('reject creating article without body @GAD-R04-01 @logged', async ({
