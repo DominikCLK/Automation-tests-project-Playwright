cbadb58f881b15075c905fe23a2db4a2c03bd55d
DominikCLK <dominik.calak@gmail.com>
2024-01-24 18:36:05 +0100

    test: go button action (API)

---
+++ tests/integration/search.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify search component for articles', () => {
+  test('go button should fetch articles @GAD-R07-01', async ({
+    articlesPage,
+    page,
+  }) => {
+    // Arrange
+    const expectDefaultArticleNumber = 6;
+    await expect(articlesPage.goSearchButton).toBeInViewport();
+    const responsePromise = page.waitForResponse('/api/articles*');
+
+    // Act
+    await articlesPage.goSearchButton.click();
+    const response = await responsePromise;
+    const body = await response.json();
+
+    // Assert
+    expect(response.ok()).toBeTruthy();
+    expect(body).toHaveLength(expectDefaultArticleNumber);
+  });
+});
