e54626ad0005828e5208fbcf20cb900fb3be5614
DominikCLK <dominik.calak@gmail.com>
2024-03-25 12:44:26 +0100

    chore: tags added for api tests

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
-test.describe('Verify articles API endpoint', () => {
+test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
   test('GET articles return status code 200', async ({ request }) => {
     // Arrange
     const expectedStatusCode = 200;
