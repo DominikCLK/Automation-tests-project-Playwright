596358412935074ffbea91241326229e4e0f988c
DominikCLK <dominik.calak@gmail.com>
2024-03-25 12:42:26 +0100

    test: api smoke articles endpoint test

---
+++ tests/api/articles.smoke.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify articles API endpoint', () => {
+  test('GET articles return status code 200', async ({ request }) => {
+    // Arrange
+    const expectedStatusCode = 200;
+
+    const articleUrl = '/api/articles';
+
+    // Act
+    const response = await request.get(articleUrl);
+
+    // Assert
+    expect(response.status()).toBe(expectedStatusCode);
+  });
+});
